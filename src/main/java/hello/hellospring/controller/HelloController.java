4ad153e02d1e9544cfca61e97605478e930b10b6
jjmin321 <jjmin321@naver.com>
2020-07-29 17:05:33 +0900

    First

---
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -0,0 +1,22 @@
+package hello.hellospring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("hello")
+    public String hello(@RequestParam("name") String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello";
+    }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam(value = "name", required = false) String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello-template";
+    }
+}
